f7f841d9085c15dab13db5d40a458fe08377268f
Matheus Vieira de Souza <101958548+Matheus-VDS@users.noreply.github.com>
2023-07-28 13:46:39 -0400

    atualização da página de análise

--- projetoAPI/Apps/homeApp/urls.py
+++ projetoAPI/Apps/homeApp/urls.py
@@ -1,20 +1,17 @@
 from django.contrib import admin
 from django.urls import path,include
 from .views import (view_data_detail,size_data,
-base,delete_data,upload_data,predict_csv_single,about,predict_csv_multi,predict_data_manually,view_data,analysis,change_password,login2,account_details,add_files_multi,detection,dashboard,userLogout,reports,upload_credit_data,prediction_button,enter_form_data_manually,add_files_single)
+base,delete_data,upload_data,predict_csv_multi,view_data,analysis,change_password,login2,add_files_multi,detection,dashboard,userLogout,reports,upload_credit_data,prediction_button)
 from . import views
 #about
 urlpatterns = [
-    #
-    #path('home/', include('Apps.homeApp.urls', namespace='home')),
-    #
+    
     
    
     path('',base),
     path('login/',login2,name='login2'),
     path('logout/',userLogout,name='userLogout'),
     path('fraud_detection/',detection,name='detection'),
-    path('about/',about,name='about'),
     path('dashboard/',dashboard,name='dashboard'),
     path('reports/',reports,name='reports'),
     path('upload_credit_data/',upload_credit_data,name='upload_credit_data'),
@@ -26,21 +23,22 @@ urlpatterns = [
     path('delete_data/<int:id>/',delete_data,name='delete_data'),
 
 
-    path('enter_form_data_manually/',enter_form_data_manually,name='enter_form_data_manually'),
-    path('add_files_single/',add_files_single,name='add_files_single'),
-    path('add_files_multi/',add_files_multi,name='add_files_multi'),
+    
+    path('add_files_multi/', views.add_files_multi, name='add_files_multi'),
 
-    path('predict_data_manually/',predict_data_manually,name='predict_data_manually'),
-    path('predict_csv_single/',predict_csv_single,name='predict_csv_single'),
-    path('predict_csv_multi/',predict_csv_multi,name='predict_csv_multi'),
 
-    path('account_details/',account_details,name='account_details'),
+    
+    
+    path('predict_csv_multi/', views.predict_csv_multi, name='predict_csv_multi'),
+    
+    
     path('change_password/',change_password,name='change_password'),
     path('analysis/<str:file_name>/', views.analysis, name='analysis'),
     path('size_data/<str:file_name>/', views.size_data, name='size_data'),
     path('view_data/', views.view_table, name='view_data'),
     path('view_data/<str:file_name>/', views.view_data_detail, name='view_data_detail'),
     
+    
 ]
 
-#path('about/',about,name='about')
+
--- projetoAPI/Apps/homeApp/views.py
+++ projetoAPI/Apps/homeApp/views.py
@@ -8,9 +8,11 @@ from django.core.paginator import Paginator
 import csv
 import os
 import pandas as pd
+import urllib.request
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.files.storage import FileSystemStorage
+from django.urls import reverse
 
 def base(request):
     return render(request,'homeApp/landing_page.html')
@@ -24,26 +26,133 @@ def reports(request):
     all_data_files_objs=DataFileUpload.objects.all()
     return render(request,'homeApp/reports.html',{'all_files':all_data_files_objs})
     
-def enter_form_data_manually(request):
-    return render(request,'homeApp/enter_form_data_manually.html')
-def predict_data_manually(request):
-    return render(request,'homeApp/predict_data_manually.html')
 
-def add_files_single(request):
-    return render(request,'homeApp/add_files_single.html')
-def predict_csv_single(request):
-    return render(request,'homeApp/predict_csv_single.html')
+
+
 
 def add_files_multi(request):
+    
+
+    if request.method == 'POST':
+        # Obter o arquivo CSV enviado pelo usuário
+        csv_file = request.FILES['csv_file']
+        validate_csv(csv_file)
+        # Obter o CPF informado pelo usuário
+        cpf = request.POST['data_file_name']
+        # Salvar o arquivo CSV no banco de dados (caso você precise fazer isso)
+        # Aqui você pode criar uma instância do modelo DataFileUpload e salvar o arquivo no campo 'actual_file_name'
+
+        # Redirecionar para a página predict_csv_multi.html, passando o nome do arquivo e o CPF como parâmetros
+        return HttpResponseRedirect(f'/predict_csv_multi/?file_name={csv_file.name}&cpf={cpf}')
+
     return render(request,'homeApp/add_files_multi.html')
+
     
 def predict_csv_multi(request):
-    return render(request,'homeApp/predict_csv_multi.html')
+    if request.method == 'GET':
+        file_name = request.GET.get('file_name')
+        cpf = request.GET.get('cpf')
 
-def account_details(request):
-    return render(request,'homeApp/account_details.html')
+        try:
+            # Fazer a requisição ao servidor para obter o conteúdo do arquivo CSV
+            with urllib.request.urlopen(file_name) as response:
+                csv_data = response.read().decode('utf-8')
+
+            df = pd.read_csv(pd.compat.StringIO(csv_data))
+            df = df.loc[df['CPF'] == int(cpf)]
+
+            df_sorted = df.sort_values(by=['CPF', 'Time'])
+            grouped = df_sorted.groupby('CPF')
+            similar_time_groups = grouped.filter(lambda x: x['CPF'].duplicated().any() and x['Time'].duplicated().any())
+
+            if (len(similar_time_groups) == 0):
+                # O DataFrame não está vazio, faça algo com os dados
+                result_data = df
+                total_amount = df['Amount'].sum()
+                max_amount = df['Amount'].max()
+                num_rows = len(df)
+                mean_amount = (total_amount - max_amount) / (num_rows - 1)
+            
+                if(7*mean_amount > max_amount):
+                     text_result = "Regular"
+                else:
+                     text_result = "Transação Irregular"
+            
+            else:
+               text_result = "Transação de tempo Irregular"
+               result_data = similar_time_groups
+            
+            context = {
+                'file_name': file_name,
+                'cpf': cpf,
+                'result_data': result_data,
+            }
+
+            return render(request, 'homeApp/predict_csv_multi.html', context)
+        except urllib.error.HTTPError as e:
+            return HttpResponse(f"Erro ao obter o arquivo: {e.code}")
+        except Exception as e:
+            return HttpResponse(f"Erro ao processar o arquivo: {str(e)}")
+
+    elif request.method == 'POST':
+        cpf = request.POST.get('data_file_name')
+        csv_file = request.FILES['csv_file']
+        text_result = " "
+        
+        with open('temp_file.csv', 'wb') as temp_file:
+            for chunk in csv_file.chunks():
+                temp_file.write(chunk)
+
+        df = pd.read_csv('temp_file.csv')
+        
+        df = df.loc[df['CPF'] == int(cpf)]
+
+        df_sorted = df.sort_values(by=['CPF', 'Time'])
+        grouped = df_sorted.groupby('CPF')
+        similar_time_groups = grouped.filter(lambda x: x['CPF'].duplicated().any() and x['Time'].duplicated().any())
+
+        if (len(similar_time_groups) == 0):
+            # O DataFrame não está vazio, faça algo com os dados
+            result_data = df
+            total_amount = df['Amount'].sum()
+            max_amount = df['Amount'].max()
+            num_rows = len(df)
+            mean_amount = (total_amount - max_amount) / (num_rows - 1)
+            
+            if(7*mean_amount > max_amount):
+               text_result = "Regular"
+            else:
+               text_result = "Transação Irregular"
+            
+        else:
+            text_result = "Transação de tempo Irregular"
+            result_data = similar_time_groups
+            
+       
+        # Clean up: delete the temporary file
+        import os
+        os.remove('temp_file.csv')
+
+        context = {
+            'file_name': csv_file.name,
+            'cpf': cpf,
+            'result_data': result_data,
+            'text_result': text_result
+        }
+
+        return render(request, 'homeApp/predict_csv_multi.html', context)
+
+
+
+    else:
+        return HttpResponse("Method not allowed.", status=405)
+            
+
+
 def change_password(request):
     return render(request,'homeApp/change_password.html')
+    
+    
 def analysis(request, file_name):
     file_path = os.path.join(settings.MEDIA_ROOT, 'uploads', file_name + '.csv')
     df = pd.read_csv(file_path)
@@ -59,14 +168,12 @@ def analysis(request, file_name):
         'total_classe_1': total_classe_1, 
     }
     return render(request, 'homeApp/analysis.html', context)
-#    return render(request,'homeApp/analysis.html')
+
+
+
 def view_data(request):
     return render(request,'homeApp/view_data.html')
-#def delete_data(request,id):
- #   obj=DataFileUpload.objects.get(id=id)
- #   obj.delete()
- #   messages.success(request, "Arquivo Deletado com successo",extra_tags = 'alert alert-success alert-dismissible show')
- #   return HttpResponseRedirect('/reports')
+
 def delete_data(request, id):
     try:
         obj = DataFileUpload.objects.get(id=id)
@@ -120,7 +227,7 @@ def upload_data(request):
         return redirect('/reports')
 
     return redirect('/reports')
-    # return HttpResponseRedirect('reports')
+    
     
 
 def userLogout(request):
@@ -150,17 +257,16 @@ def login2(request):
     else:
         return render(request, 'homeApp/login.html', data)
 
-#about
-def about(request):
-    #return render(request,'homeApp/about.html')
-    return render(request,'homeApp/about.html')
+
+
 def detection(request):
-    #return render(request,'homeApp/about.html')
+    
     return render(request,'homeApp/fraud_detection.html')
 
 def dashboard(request):
     return render(request,'homeApp/dashboard.html')
-#Função vazia sem uso {view_table}    
+    
+
 def view_table(request):
     files = YourFileModel.objects.all()  # Replace YourFileModel with your actual model
     return render(request, 'homeApp/view_data.html', {'files': files})
@@ -191,5 +297,9 @@ def size_data(request, file_name):
         'size1': size[0],
         'size2': size[1]
     }
-#    return render(request, 'homeApp/analysis.html', context)
+
     return render(request, 'homeApp/analysis.html', context)
+    
+
+    
+
